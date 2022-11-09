6de2173b74ae9cd420088cb9bbf9f808f23665b3
T0nyBlue <18521123@gm.uit.edu.vn>
2022-11-09 11:56:54 +0700

    Add Services

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,6 @@ pub struct AppState {
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     dotenv().ok();
-    // let db = PgPoolOptions::new()
-    //     .max_connections(5)
-    //     .connect(&std::env::var("DATABASE_URL").unwrap())
-    //     .await
-    //     .unwrap();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -27,7 +22,9 @@ async fn main() -> io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(AppState { db: pool.clone() }))
-            .service(web::resource("/").to(index))
+            .service(fetch_users)
+            .service(fetch_user_articles)
+            .service(create_user_article)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- src/services.rs
+++ src/services.rs
@@ -1,1 +1,42 @@
-use actix_web::{get, post, web::{Json, Path}, Responder};
+use actix_web::{get, post, web::{Json, Path}, Responder, HttpResponse};
+use serde::{Deserialize, Serialize};
+use sqlx::{self, FromRow};
+use crate::AppState;
+
+#[derive(Serialize, FromRow)]
+struct User {
+    id: i32,
+    first_name: String,
+    last_name: String,
+}
+
+#[derive(Serialize, FromRow)]
+struct Article {
+    id: i32,
+    title: String,
+    content: String,
+    create_by: i32,
+}
+
+#[derive(Deserialize)]
+pub struct CreateArticleBody {
+    title: String,
+    content: String,
+}
+
+#[get("/users")]
+pub async fn fetch_users() -> impl Responder {
+    "GET /users".to_string()
+}
+
+#[get("/users/{id}/articles")]
+pub async fn fetch_user_articles(path: Path<i32>) -> impl Responder {
+  let id: i32 = path.into_inner();  
+  format!("GET /users/{}/articles")
+}
+
+#[post("/users/{id}/articles")]
+pub async fn create_user_article(path: Path<i32>, body: Json<CreateArticleBody>) -> impl Responder {
+  let id: i32 = path.into_inner();
+  format!("POST /users/{}/articles")
+}
